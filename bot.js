a4bf8df6699f8ddf382fa4882abac3ced2c64862
AhmedElgimizy <43325089+AhmedElgimizy@users.noreply.github.com>
2018-10-18 18:40:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const Util = require('discord.js');
 
 const getYoutubeID = require('get-youtube-id');
 
-const fetchVideoInfo = require('youtube-info');
-
 const YouTube = require('simple-youtube-api');
 
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
